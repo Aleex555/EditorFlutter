392a19af17337c639ee13d05bccfe1252bd52acb
Aleex555 <amartinezgonzalez.cf@iesesteveterradas.cat>
2024-01-06 17:37:49 +0100

    .

---
+++ editor_base/lib/util_shape.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Shape {
+  Offset position = const Offset(0, 0);
+  Size scale = const Size(1, 1);
+  double rotation = 0;
+  List<Offset> vertices = [];
+
+  double stroke = 1;
+  Color color = Colors.black;
+
+  Shape();
+
+  void setColor(Color newColor) {
+    color = newColor;
+  }
+
+  void setStrokeWidth(double size) {
+    stroke = size;
+  }
+
+  void setPosition(Offset newPosition) {
+    position = newPosition;
+  }
+
+  void setScale(Size newScale) {
+    scale = newScale;
+  }
+
+  void setRotation(double newRotation) {
+    rotation = newRotation;
+  }
+
+  void addPoint(Offset point) {
+    vertices.add(Offset(point.dx, point.dy));
+  }
+
+  void addRelativePoint(Offset point) {
+    vertices.add(Offset(point.dx - position.dx, point.dy - position.dy));
+  }
+
+// Converteix la forma en un mapa per serialitzar
+ Map<String, dynamic> toMap() {
+   return {
+     'type': 'shape_drawing',
+     'object': {
+       'position': {'dx': position.dx, 'dy': position.dy},
+       'vertices': vertices.map((v) => {'dx': v.dx, 'dy': v.dy}).toList(),
+       'strokeWidth': stroke,
+       'strokeColor': color.value, 
+     }
+   };
+ }
+
+ // Crea una forma a partir d'un mapa
+ static Shape fromMap(Map<String,dynamic> map) {
+   if (map['type'] != 'shape_drawing') {
+     throw Exception('Type is not a shape_drawing');
+   }
+
+   var objectMap = map['object'] as Map<String, dynamic>;
+   var shape = Shape()
+     ..setPosition(
+         Offset(objectMap['position']['dx'], objectMap['position']['dy']))
+     ..setStrokeWidth(objectMap['strokeWidth'])
+     ..setColor(Color(objectMap['strokeColor']));
+
+   if (objectMap['vertices'] != null) {
+     var verticesList = objectMap['vertices'] as List;
+     shape.vertices =
+         verticesList.map((v) => Offset(v['dx'], v['dy'])).toList();
+   }
+
+   return shape;
+ }
+
+
+}
